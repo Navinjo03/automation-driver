dfa8bb4db8b2a49fbfa04c385314a805788998c1
Navinjo03 <s.n.jones0304@gmail.com>
2024-04-02 20:38:31 +0530

    Module changes

---
+++ web-driver/src/main/java/com/webDriver/element/locator/relative_locator/service/RelativeLocator.java
@@ -0,0 +1,17 @@
+package com.webDriver.element.locator.relative_locator.service;
+
+
+import com.webDriver.element.locator.Locators;
+import com.webDriver.element.web.element.service.PageElement;
+
+public interface RelativeLocator {
+
+     RelativeLocator above(final PageElement element);
+     RelativeLocator above(final Locators locator);
+     RelativeLocator below(final PageElement element);
+     RelativeLocator below(final Locators locator);
+     RelativeLocator toRightOf(final PageElement element);
+     RelativeLocator toRightOf(final Locators locator);
+     RelativeLocator toLeftOf(final PageElement element);
+     RelativeLocator toLeftOf(final Locators locator);
+}
